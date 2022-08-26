c003a4c3cf6f56e8158d8bbf5615eceb121b546d
loctraSmc <109941572+loctraSmc@users.noreply.github.com>
2022-08-26 10:50:40 +0400

    update release tag

--- buildSrc/src/releases.kt
+++ buildSrc/src/releases.kt
@@ -2,6 +2,5 @@ const val SDK_MIN = 14
 const val SDK_TARGET = 31
 
 const val RELEASE_GROUP = "com.lt.hastagview"
-const val RELEASE_ARTIFACT = "socialview"
-const val RELEASE_VERSION = "0.1-SNAPSHOT"
+const val RELEASE_VERSION = "1.0.0"
 const val RELEASE_DESCRIPTION = "Android TextView and EditText with hashtag, mention, and hyperlink support"
